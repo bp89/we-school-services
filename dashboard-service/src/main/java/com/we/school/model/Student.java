0b22c81b4baf041798e1852e69adcf6982ecdf38
vip <vinayprajapati107@gmail.com>
2025-01-28 17:40:20 +0000

    initial commit

--- dashboard-service/src/main/java/com/we/school/model/Student.java
+++ dashboard-service/src/main/java/com/we/school/model/Student.java
@@ -9,7 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.antlr.v4.runtime.misc.NotNull;
+import org.antlr.v4.runtime.misc.NotN     ull;
 
 @Data
 @Builder
